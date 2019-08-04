b87c09a56d2398974796bd3c05c845717f4f06cb
premanshum <premanshu.mukherji@gmail.com>
2019-08-04 23:08:54 +0530

    added msal;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 //import { HttpModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { InvoiceService } from './Services/InvoiceService';
 import { InvoiceListComponent } from './invoices/invoice-list.component';
@@ -21,6 +22,20 @@ import { ModalBox} from './reUsables/modalBox/modalBox';
 import { ModalTriggerDirective} from './reUsables/modalBox/modalBox-trigger-directive';
 import { appRoutes } from './routes';
 import { JQ_TOKEN, JQUERY_PROVIDER } from './services/jQuery-service';
+
+import { MsalModule, MsalGuard, MsalInterceptor, MsalService } from '@azure/msal-angular';
+import { LogLevel } from 'msal';
+
+// Logger callback for MSAL
+export function loggerCallback(logLevel, message, piiEnabled) {
+  console.log(message);
+}
+
+// Note - locally we're using http for Azure Functions.  
+// This is needed for the interceptor to attach tokens when making calls to the Function API.
+export const protectedResourceMap: [string, string[]][] = [["http://localhost:4200/invoices", 
+                        ['api://d24d28b4-09c9-4230-a9a0-ef113169c7b2/access_as_user_all']]];
+
 
 @NgModule({
   declarations: [
@@ -40,11 +55,32 @@ import { JQ_TOKEN, JQUERY_PROVIDER } from './services/jQuery-service';
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MsalModule.forRoot({
+      authority: "https://login.microsoftonline.com/common/",
+      consentScopes: [ "access_as_user_all", "api://d24d28b4-09c9-4230-a9a0-ef113169c7b2/access_as_user_all"],
+      clientID: "d24d28b4-09c9-4230-a9a0-ef113169c7b2",
+      redirectUri: "http://localhost:4200/invoices",
+      popUp: true,
+      protectedResourceMap: protectedResourceMap,
+      postLogoutRedirectUri: "http://localhost:4200/",
+      logger: loggerCallback,
+      level: LogLevel.Verbose
+    }),
+    RouterModule.forRoot([
+      { path: 'invoices', 
+        component: InvoiceListComponent, 
+        canActivate: [MsalGuard] },
+    ])
   ],
   providers: [
     InvoiceService,
-    JQUERY_PROVIDER
+    JQUERY_PROVIDER,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [InvoiceApp],
   
